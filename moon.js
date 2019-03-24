6aca016cc978e2ec7644c90ad0badaeab8be22d0
gegelascience <remi.verchere2@gmail.com>
2019-03-24 17:23:09 +0100

    use less resouces

--- moon.js
+++ moon.js
@@ -3,28 +3,44 @@ AFRAME.registerComponent("moon", {
     tRevol: { type: "number", default: 500000 },
     axe: { type: "number", default: 0.4 },
     angularStart: { type: "number", default: 0 },
-    tRotation: { type: "number", default: 5000 }
+    tRotation: { type: "number", default: 5000 },
+    timeEvol: { type: "number", default: 0 },
+    isEnable: { type: "boolean", default: false }
   },
-  init: function() {},
+  init: function() {
+    document
+      .getElementById("marker_hiro")
+      .addEventListener("markerFound", () => {
+        this.data.isEnable = true;
+      });
+    document
+      .getElementById("marker_hiro")
+      .addEventListener("markerLost", () => {
+        this.data.isEnable = false;
+      });
+  },
 
   tick: function(time, timeDelta) {
     // Do something on every scene tick or frame.
-    var pos = this.el.getAttribute("position");
-    pos.x =
-      2 *
-      this.data.axe *
-      Math.cos(
-        this.data.angularStart + (360 * (time + timeDelta)) / this.data.tRevol
-      );
-    pos.z =
-      2 *
-      this.data.axe *
-      Math.sin(
-        this.data.angularStart + (360 * (time + timeDelta)) / this.data.tRevol
-      );
-    this.el.setAttribute("position", pos);
-    var rot = this.el.getAttribute("rotation");
-    rot.y = (360 * (time + timeDelta)) / this.data.tRotation;
-    this.el.setAttribute("rotation", rot);
+    if (this.data.isEnable) {
+      this.data.timeEvol += timeDelta;
+      var pos = this.el.getAttribute("position");
+      pos.x =
+        2 *
+        this.data.axe *
+        Math.cos(
+          this.data.angularStart + (360 * this.data.timeEvol) / this.data.tRevol
+        );
+      pos.z =
+        2 *
+        this.data.axe *
+        Math.sin(
+          this.data.angularStart + (360 * this.data.timeEvol) / this.data.tRevol
+        );
+      this.el.setAttribute("position", pos);
+      var rot = this.el.getAttribute("rotation");
+      rot.y = (360 * this.data.timeEvol) / this.data.tRotation;
+      this.el.setAttribute("rotation", rot);
+    }
   }
 });
